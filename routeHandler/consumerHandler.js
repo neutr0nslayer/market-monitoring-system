4d56bf9d3e10ce8d40fb8e0c709751c0c9ea90d7
Likhon <likhon.929@gmail.com>
2025-02-22 00:32:43 +0600

    feat: add complaint submission feature for consumers with new route and view

--- routeHandler/consumerHandler.js
+++ routeHandler/consumerHandler.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const complaintSchema = require('../schemas/complaintSchema');
+const userSchema = require('../schemas/userSchema');
 const { authenticateConsumer } = require('../middlewares/authMiddleware');
 
 // Create a Complaint model
 const Complaint = mongoose.model('Complaint', complaintSchema);
+const User = mongoose.model('User', userSchema);
 
 // Add this route to fetch consumer's complaints
 router.get('/dashboard', authenticateConsumer, async (req, res) => {
@@ -13,6 +15,22 @@ router.get('/dashboard', authenticateConsumer, async (req, res) => {
         const complaints = await Complaint.find({ consumerId: req.user.userId });
 
         res.render('consumerDashboard', { complaints });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error while fetching dashboard data' });
+    }
+});
+
+//
+router.get('/submit-complaint', authenticateConsumer, async (req, res) => {
+    try {
+        const consumerId = req.user.userId;
+        const companies = await User.aggregate([
+            { $match: { role: 'company' } },
+            { $group: { _id: "$companyDetails.name", userId: { $first: "$_id" } } }
+        ]);
+
+        res.render('submitComplaint', { consumerId, companies });
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Error while fetching dashboard data' });
